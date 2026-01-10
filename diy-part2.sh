8c5d3b513ffdc73580302f38cc7ab248eef8e891
Xmqor <90999387+Xmqor@users.noreply.github.com>
2026-01-10 10:47:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,7 @@
 
 # 复制nanopi-t4的patch到patches目录
 cp -f $GITHUB_WORKSPACE/kernel-rockchip/patches/*.patch target/linux/rockchip/patches-6.6/
+cp -f ${GITHUB_WORKSPACE}/patch/T4/fix-CPU-information-6.16.patch patch/kernel/archive/rockchip64-6.6/
+# cp -f ${GITHUB_WORKSPACE}/patch/T4/fix-CPU-information-6.16.patch patch/kernel/archive/rockchip64-6.19/
+# cp -f ${GITHUB_WORKSPACE}/patch/T4/t4.patch patch/kernel/archive/rockchip64-6.18/
+cp -f ${GITHUB_WORKSPACE}/patch/T4/t4.patch patch/kernel/archive/rockchip64-6.6/
